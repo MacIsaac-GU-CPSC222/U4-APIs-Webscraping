2c6bbf0b58a9e90de5edba95c6adb3ee0292fa6d
Dominic MacIsaac <dmacisaac@ewu.edu>
2026-02-25 10:17:03 -0800

    added task answer

--- JSONFun/json_testing_complete.py
+++ JSONFun/json_testing_complete.py
@@ -2,6 +2,7 @@ import json
 
 j_file = open("climbing.json")
 obj = json.load(j_file)
+j_file.close()
 print(type(obj))
 print(obj["climber"])
 # get the climber's name
@@ -17,4 +18,8 @@ print(obj["notes"])
 print(obj["total_time_minutes"])
 
 # Task! Print out how many total attempts he completed
+attempts = 0
+for route in obj["routes"]:
+    attempts += route["attempts"]
 
+print(f"total attempts: {attempts}")
