7ce3697a69ba054f1b03fda46d546739d0d40f94
Dominic MacIsaac <dmacisaac@ewu.edu>
2026-03-04 10:04:38 -0800

    scraping demo update

--- APIFun/bball_scraping.py
+++ APIFun/bball_scraping.py
@@ -4,3 +4,15 @@ import pandas as pd
 
 url = "https://www.allmysportsteamssuck.com/ncaa-division-i-football-and-basketball-twitter-hashtags-and-handles/"
 
+
+
+
+
+
+
+
+
+# step 4 TASK: create a pandas dataframe for this table data
+# then, parse the thead element to get the names of the columns
+# and make these your dataframe column names
+# then write the dataframe to a file
--- APIFun/bball_scraping_complete.py
+++ APIFun/bball_scraping_complete.py
@@ -7,15 +7,17 @@ url = "https://www.allmysportsteamssuck.com/ncaa-division-i-football-and-basketb
 response = requests.get(url)
 # print(response.text)
 soup = BeautifulSoup(response.text, "html.parser")
-# print(soup)
+print(type(soup))
 # # step 1: get the table element (AKA tag)
 table = soup.find("table", attrs={"id": "rankingstable"})
 # print(table)
 print(type(table))
 # # step 2: grab the tbody's table rows (trs)
 tbody = table.find("tbody")
+print(type(tbody))
 
 trs = tbody.find_all("tr")
+
 print(type(trs))
 print(len(trs))
 # # step 3: process each table row (tr) by getting its data (tds)
@@ -23,12 +25,28 @@ rows = []
 for tr in trs:
     row = []
     tds = tr.find_all("td")
-    print(tds)
+    # print(tds)
     for td in tds:
-        print(td.get_text())
+        # print(td.get_text())
         # print(td.get_text(), end="\t")
         row.append(td.get_text())
-    print()
+    # print()
     rows.append(row)
 
-print(rows)
+# print(rows)
+# step 4 TASK: create a pandas dataframe for this table data
+# then, parse the thead element to get the names of the columns
+# and make these your dataframe column names
+# then write the dataframe to a file
+
+thead = table.find("thead")
+ths = thead.find_all("th")
+header = []
+for th in ths:
+    header.append(th.get_text())
+
+# print(header, rows)
+df = pd.DataFrame(rows, columns=header)
+df = df.set_index("School")
+print(df)
+df.to_csv("bball_twitter_names.csv")
